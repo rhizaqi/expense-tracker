7f676706938c74b0d086080a93303ed62e2b1d58
Oky <146697026+rhizaqi@users.noreply.github.com>
2025-08-19 09:58:17 +0700

    feat: some frontend

---
+++ fe-expense-tracker/src/pages/Auth/Dashboard/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home  ()  {
+
+    return (
+        <>
+        this is Home page
+        </>
+    )
+} 
---
+++ fe-expense-tracker/src/pages/Auth/Dashboard/Income.jsx
@@ -0,0 +1,8 @@
+export default function Income  ()  {
+
+    return (
+        <>
+        this is Income page
+        </>
+    )
+} 
