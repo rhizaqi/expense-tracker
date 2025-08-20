38ff37fc49605e353d1dd7970c82efce50fe21cb
Oki <rhizaqi94@gmail.com>
2025-08-20 21:06:15 +0700

    feat: starting do some login

--- fe-expense-tracker/src/pages/Auth/Login.jsx
+++ fe-expense-tracker/src/pages/Auth/Login.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/inputs/input";
 
 export default function Login() {
-    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  //Handle Login Form Submit
+
+  const handleLogin = async () => {};
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -9,6 +20,24 @@ export default function Login() {
         <p className="text-xs text-slate-700 mt-[5px] mb">
           Please enter your details to login
         </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Minimal 8 Characters"
+            type="password"
+          />
+        </form>
       </div>
     </AuthLayout>
   );
