bd2385e346dc16e3cf6bb5ced587d36b5937486d
Oki <rhizaqi94@gmail.com>
2025-08-28 18:49:21 +0700

    feat: success add new income

--- be-expense-tracker/controllers/incomeController.js
+++ be-expense-tracker/controllers/incomeController.js
@@ -1,5 +1,36 @@
+const Income = require("../models/Income");
+const User = require("../models/User");
+
 //Add income source
-exports.addIncome = async (req, res) => {};
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const { icon, source, amount, date } = req.body;
+
+    if (!source || !icon || !amount || !date) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(),
+    });
+
+    await newIncome.save();
+
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
 
 //Get all income source
 exports.getAllIncome = async (req, res) => {};
