b039df715e8c602fb5ff35eb87db99e52fd39b86
Oki <rhizaqi94@gmail.com>
2025-09-01 12:58:41 +0700

    feat: create api path and axios instance

---
+++ fe-expense-tracker/src/utility/axiosInstance.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPath";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+
+axiosInstance.interceptors.request.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.log("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.log("Request timeout. Please try again");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
