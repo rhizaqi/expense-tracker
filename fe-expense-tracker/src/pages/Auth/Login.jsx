76e1e2c6facc7d75b6900bde0f203984a7201fb4
Oki <rhizaqi94@gmail.com>
2025-09-01 14:09:08 +0700

    feat: login using own api

--- fe-expense-tracker/src/pages/Auth/Login.jsx
+++ fe-expense-tracker/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/inputs/input";
 import { validateEmail } from "../../utility/helper";
+import axiosInstance from "../../utility/axiosInstance";
+import { API_PATH } from "../../utility/apiPath";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -29,6 +31,26 @@ export default function Login() {
     setError("");
 
     //Login API Call
+
+    try {
+      const response = await axiosInstance.post(API_PATH.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
   };
 
   return (
